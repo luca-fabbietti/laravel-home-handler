a5f867fb97309ae8ec575216bb73f039c43dc9cb
luca-fabbietti <lucafabbietti@yahoo.it>
2025-06-26 15:30:08 +0200

    bugfix

--- infra/certbot/certbot-boot.sh
+++ infra/certbot/certbot-boot.sh
@@ -1,7 +1,7 @@
 docker run --rm \
   -p 80:80 \
-  -v "$(pwd)/infra/certbot/conf:/etc/letsencrypt" \
-  -v "$(pwd)/infra/certbot/www:/var/www/certbot" \
+  -v "$(pwd)/conf:/etc/letsencrypt" \
+  -v "$(pwd)/www:/var/www/certbot" \
   certbot/certbot certonly \
     --standalone \
     --preferred-challenges http \
