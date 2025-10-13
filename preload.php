be6464cb4ae9c63b854f0ac96446f9470977a6cf
luca-fabbietti <110847212+luca-fabbietti@users.noreply.github.com>
2025-10-13 20:13:42 +0000

    fix code style

--- preload.php
+++ preload.php
@@ -1,8 +1,9 @@
 <?php
+
 // Preload Composer's autoload
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 // Preload Laravel core files
-foreach (glob(__DIR__ . '/vendor/laravel/framework/src/Illuminate/**/*.php') as $file) {
+foreach (glob(__DIR__.'/vendor/laravel/framework/src/Illuminate/**/*.php') as $file) {
     opcache_compile_file($file);
 }
