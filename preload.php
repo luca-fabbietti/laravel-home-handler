f9407b9940a29f1446c79f14bbcb72fdd2c88ed7
luca-fabbietti <110847212+luca-fabbietti@users.noreply.github.com>
2026-02-12 19:28:14 +0000

    fix code style

--- preload.php
+++ preload.php
@@ -1,10 +1,11 @@
 <?php
+
 // Preload Composer's autoload
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 // Preload all vendor PHP files
 $iterator = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator(__DIR__ . '/vendor', RecursiveDirectoryIterator::SKIP_DOTS)
+    new RecursiveDirectoryIterator(__DIR__.'/vendor', RecursiveDirectoryIterator::SKIP_DOTS)
 );
 
 foreach ($iterator as $file) {
